a5bea599fea6925209ea022b7402ed71f5107f5f
mzvast <mzvast@gmail.com>
2022-09-29 21:17:02 +0800

    isogram update

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -3,15 +3,9 @@ use std::collections::HashSet;
 pub fn check(candidate: &str) -> bool {
     // unimplemented!("Is {} an isogram?", candidate);
     let mut h = HashSet::<char>::new();
-    for c in candidate
+    candidate
+        .to_lowercase()
         .chars()
         .filter(|x| x.is_alphabetic())
-        .map(|c| c.to_ascii_lowercase())
-    {
-        if h.contains(&c) {
-            return false;
-        }
-        h.insert(c);
-    }
-    true
+        .all(|c| h.insert(c))
 }
